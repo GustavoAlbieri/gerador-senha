5d05f6adcbcc8d10b0d45c2e13c6617a5af4cf9a
Gustavo <gustavo.albieri1999@gmail.com>
2022-05-10 10:28:19 -0300

    primerio commit

---
+++ script.js
@@ -0,0 +1,33 @@
+let sliderElement = document.querySelector('#slider');
+let buttonElement = document.querySelector('#button');
+
+let sizePassword = document.getElementById("valor");
+let password = document.getElementById("password");
+
+let containerPassword = document.querySelector("#container-password");
+
+let charset = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM123456789.!@#$%&*";
+let novasenha = "";
+
+sizePassword.innerHTML= sliderElement.value;
+
+slider.oninput = function() {
+    sizePassword.innerHTML = this.value;
+}
+function gerar() {
+
+    let pass = "";
+    for(let i = 0, n = charset.length; i < sliderElement.value; i++) {
+        pass += charset.charAt(Math.floor(Math.random() * n));
+    }
+
+    containerPassword.classList.remove("hide");
+    password.innerHTML = pass;
+
+}
+
+function copiar() {
+    alert("Senha copiada com sucesso!");
+    navigator.clipboard.writeText(novasenha);
+}
+
